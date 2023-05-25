6ac1172df749936008141a5cbf3b8086e2f6f4eb
Aiden <devaiden923@gmail.com>
2023-05-25 19:32:16 +0900

    Update 2108-통계학.cpp

--- boj/2108-통계학.cpp
+++ boj/2108-통계학.cpp
@@ -13,19 +13,17 @@ int main()
     cout.tie(NULL);
 
     int n;
-    cin >> n; // 
+    cin >> n;
 
     vector<int> arr(n);
-    // 숫자의 빈도를 저장할 map 선언
     map<int, int> freq; 
 
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
-        freq[arr[i]]++; // 빈도 map에 해당 숫자의 빈도를 증가
+        freq[arr[i]]++; 
     }
 
-    // 평균 계산
     double avg = 0;
     for (auto num : arr)
     {
@@ -33,35 +31,32 @@ int main()
     }
     avg /= n;
 
-    // 정렬을 통해 중앙값과 범위를 구한다
     sort(arr.begin(), arr.end());
-    int median = arr[n / 2]; // 중앙값
-    int range = arr[n - 1] - arr[0]; // 범위
+    int median = arr[n / 2]; 
+    int range = arr[n - 1] - arr[0]; 
 
     // 최빈값을 구한다
     int mode = 0;
     int max_freq = 0;
-    bool second = false;
+    int cnt = 0;
 
     for (auto& elem : freq)
     {
-        // 최대 빈도가 같을 때, 두 번째 최빈값을 구한다
-        if (max_freq == elem.second)
-        {
-            if (!second)
-            {
-                mode = elem.first;
-                second = true;
-            }
-        }
-
         // 최대 빈도가 현재 elem.second보다 작을 때
         if (max_freq < elem.second)
         {
             max_freq = elem.second;
             mode = elem.first;
-            // 두 번째 최빈값을 다시 false로 설정
-            second = false; 
+            cnt = 0;
+        }
+        // 최대 빈도가 같을 때, 두 번째로 작은 최빈값을 구한다
+        else if (max_freq == elem.second)
+        {
+            if (cnt == 0)
+            {
+                mode = elem.first;
+                cnt++;
+            }
         }
     }
 
