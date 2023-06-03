c9a32ea8bfe13e6e92568d53470477bd74b5628a
Minsung Kim <134011277+AidenKim923@users.noreply.github.com>
2023-06-03 15:37:42 +0900

    백준허브,릿코드허브를 위해 제거

--- boj/1065-한수.py
+++
@@ -1,3 +0,0 @@
-n = int(input())
-print(sum([(n < 100) or (n // 100 + n%10 == 2*(n//10 % 10)) for n in range(1, n+1)]))
-
--- boj/2902-KMP.py
+++
@@ -1,6 +0,0 @@
-n = input().split("-")
-
-for i in n:
-    print(i[0], end='')
-    
-    
