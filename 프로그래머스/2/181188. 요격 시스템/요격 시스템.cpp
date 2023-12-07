8d0ba53dff8d81e257ba3acef14affab75ea7547
Aiden <devaiden923@gmail.com>
2023-12-07 10:23:19 +0900

    [level 2] Title: 요격 시스템, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(vector<int> A, vector<int> B)
+{
+    if (A[1] == B[1])
+    {
+        return A[0] < B[0];
+    }
+    return A[1] < B[1];
+}
+
+int solution(vector<vector<int>> targets) {
+    int answer = 1;
+
+    sort(targets.begin(), targets.end(), cmp);
+
+    int end = targets[0][1];
+
+    for (int i = 1; targets.size() > i; ++i)
+    {
+        if (targets[i][0] >= end)
+        {
+			end = targets[i][1];
+			answer++;
+		}
+    }
+
+    return answer;
+}
