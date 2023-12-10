60992af708ef3b120e805e2ae9fcf975280d912f
Aiden <devaiden923@gmail.com>
2023-12-10 14:51:36 +0900

    [level 3] Title: 정수 삼각형, Time: 0.91 ms, Memory: 8.83 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -5,29 +5,29 @@
 using namespace std;
 
 int solution(vector<vector<int>> triangle) {
-    int answer = 0;
-    int dp[501][501] = { 0, };
     int n = triangle.size();
+    vector<vector<int>> dp(n, vector<int>(n, 0));
     dp[0][0] = triangle[0][0];
 
-    for (int i = 1; n > i; ++i)
+    for (int i = 1; i < n; ++i) 
     {
-        for (int j = 0; i >= j; ++j)
+        for (int j = 0; j <= i; ++j) 
         {
-            if (j == 0) dp[i][j] = dp[i - 1][0] + triangle[i][j];
-            else if (j == i) dp[i][j] = dp[i - 1][i - 1] + triangle[i][j];
-            else dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
-
+            if (j == 0) 
+            {
+                dp[i][j] = dp[i - 1][0] + triangle[i][j];
+            }
+            else if (j == i) 
+            {
+                dp[i][j] = dp[i - 1][i - 1] + triangle[i][j];
+            }
+            else 
+            {
+                dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
+            }
         }
     }
 
-    for (int i = 0; n > i; ++i)
-    {
-        if (answer < dp[n - 1][i])
-        {
-            answer = dp[n - 1][i];
-        }
-    }
-
+    int answer = *max_element(dp[n - 1].begin(), dp[n - 1].end());
     return answer;
 }
