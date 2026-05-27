8df36666213ddf997b67f20ff62d94e5da787cfc
aurenixs <devaiden923@gmail.com>
2026-05-27 12:14:11 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

---
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> players, int m, int k) {
+    int answer = 0;
+    int server = 0;
+    queue<pair<int, int>> time;
+
+    for (int i = 0; i < players.size(); ++i)
+    {
+        if (!time.empty() && (time.front().first == i))
+        {
+            server -= time.front().second;
+            time.pop();
+        }
+
+        if ( m <= players[i])
+        {
+            int required = players[i] / m;
+
+            if (required > server)
+            {
+                int newServer = required - server;
+                answer += newServer;
+                server += newServer;
+
+                time.push(make_pair(i + k, newServer));
+            }
+        }
+    }
+    return answer;
+}
