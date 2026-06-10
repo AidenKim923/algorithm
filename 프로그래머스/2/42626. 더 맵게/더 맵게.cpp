f8bffc4e8963fc94434dc3d79998b30aa58724cf
aurenixs <devaiden923@gmail.com>
2026-06-10 15:01:42 +0900

    [level 2] Title: 더 맵게, Time: 182.05 ms, Memory: 40.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
+
+    int count = 0;
+
+    while (pq.top() < K)
+    {
+        if (pq.size() < 2) return -1;
+
+        int first = pq.top();
+        pq.pop();
+
+        int second = pq.top();
+        pq.pop();
+
+        pq.push(first + second * 2);
+        ++count;
+    }
+
+    return count;
+}
